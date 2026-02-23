7d5a774be0bfd0e513d2b416cf9ee57b7e32393a
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-02-23 17:48:44 +0500

    if else condtion

---
+++ day1.js
@@ -0,0 +1,87 @@
+console.log("Mera Naam Zohaib Hai");
+
+//STRING - DATA TYPE
+
+
+console.log("Mera Naam Ali Hai");
+
+console.log("30 + HELLO KESE HO - 20")
+
+
+// NUMBER - DATA TYPE
+
+console.log(40 * 20, "Mai yahan 40 x 20 kar raha hun")
+console.log(400 * 2, "Mai yahan 400 x 2 kar raha hun")
+
+
+console.log("40 - 20")
+
+
+//BOOLEAN - DATA 
+//  TRUE
+//  FALSE
+
+
+// IF ELSE CONDITION
+var citizenAge = 110;
+
+var minimumAge = 18;
+
+// = 
+
+// 18 = 18
+
+
+
+
+if (citizenAge == minimumAge) {
+    console.log("CNIC BAN SAKTA HAI")
+} else {
+    console.log("CNIC NAHI BAN SAKTA")
+}
+
+
+// var
+// let
+// const
+
+
+
+let userEmail = "zohaibsoomro0101@gmail.net"
+
+let originalEmail = "zohaibsoomro001@gmail.net"
+
+
+let testingEmail = userEmail == originalEmail;
+
+console.log(testingEmail)
+
+
+if (userEmail == originalEmail) {
+    console.log("Login")
+} else {
+    console.log("Invalid Credentials")
+}
+
+
+let userAge = 18;
+
+
+
+if (userAge >= 18) {
+    console.log("APP CNIC KAY LIYE ELIGIBLE HAIN")
+} else {
+    console.log("APP CNIC KAY LIYE ELIGIBLE NAHI HAIN")
+}
+
+
+
+let number = -0.1;
+
+if (number > 0) {
+    console.log("POSITIVE NUMBER")
+} else if (number < 0) {
+    console.log("NEGATIVE NUMBER")
+} else {
+    console.log("ZEROO")
+}
